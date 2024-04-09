346da24de5e20973860e5846acaf9680f88c9d17
ManuGT <eventura@student.unimelb.edu.au>
2024-04-09 18:59:24 +1000

    Save only necessary properties

--- src/core/save.c
+++ src/core/save.c
@@ -134,7 +134,7 @@ void calc_hdf5_props()
     galaxy_output_t galout;
     int i; // dummy
 
-    h5props->n_props = 49;
+    h5props->n_props = 3;
 #if USE_MINI_HALOS
     h5props->n_props += 14; // Double check later
 #endif
