7e499fe69a0a607690ce32c2defc648f9cd87c0b
ManuGT <eventura@student.unimelb.edu.au>
2024-04-23 09:54:07 +1000

    Found mistake in the atomic cooling limit

--- src/core/reionization.c
+++ src/core/reionization.c
@@ -1415,9 +1415,8 @@ void construct_baryon_grids(int snapshot, int local_ngals)
   float* sfrMCIII_grid = run_globals.reion_grids.sfrMCIII;
   float* sfrMC_grid = run_globals.reion_grids.sfrMC;
   
-  double zplus1 = run_globals.ZZ[snapshot] + 1;
-  //double MatoLim = 5.4 * 1e-3 * 0.6751 * pow(zplus1 / 11.0, -1.5); // Probably it doesn't take little_h!!
-  double MatoLim = 3.1623 * 1e-3; // Test for low mass AC halos above z = 20. It should have little_h but don't put it now for consistency!
+  double redshift = run_globals.ZZ[snapshot];
+  double MatoLim = Tvir_to_Mvir(1e4, redshift);
 #endif
 
   gal_to_slab_t* galaxy_to_slab_map = run_globals.reion_grids.galaxy_to_slab_map;
