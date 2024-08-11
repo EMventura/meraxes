c5e05c69f19c4e579cce9542582f9ff663abd35f
ManuGT <eventura@student.unimelb.edu.au>
2024-08-11 11:13:13 +1000

    Error in the heating. You were not accounting for PopIII contribution

--- src/core/XRayHeatingFunctions.c
+++ src/core/XRayHeatingFunctions.c
@@ -1385,9 +1385,12 @@ void evolveInt(float zp,
   dspec_dzp_II = -dxe_dzp * TII / (1 + x_e);
 
   dcomp_dzp_II = dT_comp(zp, TII, x_e);
-
-  dxheat_dzp = (dxheat_dt_GAL + dxheat_dt_III) * dt_dzp * 2.0 / 3.0 / BOLTZMANN / (1.0 + x_e);
+ 
   dxheat_dzp_II = dxheat_dt_GAL * dt_dzp * 2.0 / 3.0 / BOLTZMANN / (1.0 + x_e);
+#endif
+
+#if USE_MINI_HALOS || USE_SCALING_REL
+  dxheat_dzp = (dxheat_dt_GAL + dxheat_dt_III) * dt_dzp * 2.0 / 3.0 / BOLTZMANN / (1.0 + x_e);
 #else
   dxheat_dzp = dxheat_dt_GAL * dt_dzp * 2.0 / 3.0 / BOLTZMANN / (1.0 + x_e);
 #endif
