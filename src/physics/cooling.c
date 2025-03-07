e94990e0d38379d50517317a086b29dfd04c6cf7
ManuGT <eventura@student.unimelb.edu.au>
2025-03-07 16:28:58 +1100

    Add an extra check just to be extra sure

--- src/physics/cooling.c
+++ src/physics/cooling.c
@@ -130,24 +130,26 @@ void cool_gas_onto_galaxy(galaxy_t* gal, double cooling_mass)
   gal->Mcool = cooling_mass;
   
 #if USE_ANG_MOM
-  // update the gas disk size and velocity. assumes that V=Vvir, j=jhalo and
-  // R=j/2V
-  double AMcool[3]; // This is total!
-  // Doing this just because you found a stupid float/double bug!
-  double newAngMom[3] = { gal->Halo->AngMom[0], gal->Halo->AngMom[1], gal->Halo->AngMom[2] };
-  specific_to_total_angmom(newAngMom, cooling_mass, AMcool);
-  //specific_to_total_angmom(gal->Halo->AngMom, cooling_mass, AMcool);
-  // Do this for now.
-  /*double HaloAngMomMag = sqrt(gal->Halo->AngMom[0] * gal->Halo->AngMom[0] + 
-                          gal->Halo->AngMom[1] * gal->Halo->AngMom[1] +
-                          gal->Halo->AngMom[2] * gal->Halo->AngMom[2]);*/
-  // vector_magnitude needs a double vector, not a float!!!
-  add_disks(gal, 1, cooling_mass,
-            vector_magnitude(newAngMom) / (2 * gal->Vvir),
-            gal->Vvir, AMcool);
-  /*add_disks(gal, 1, cooling_mass,
-            HaloAngMomMag / (2 * gal->Vvir),
-            gal->Vvir, AMcool);*/
+  if (cooling_mass >= 1e-10) {
+    // update the gas disk size and velocity. assumes that V=Vvir, j=jhalo and
+    // R=j/2V
+    double AMcool[3]; // This is total!
+    // Doing this just because you found a stupid float/double bug!
+    double newAngMom[3] = { gal->Halo->AngMom[0], gal->Halo->AngMom[1], gal->Halo->AngMom[2] };
+    specific_to_total_angmom(newAngMom, cooling_mass, AMcool);
+    //specific_to_total_angmom(gal->Halo->AngMom, cooling_mass, AMcool);
+    // Do this for now.
+    /*double HaloAngMomMag = sqrt(gal->Halo->AngMom[0] * gal->Halo->AngMom[0] + 
+                            gal->Halo->AngMom[1] * gal->Halo->AngMom[1] +
+                            gal->Halo->AngMom[2] * gal->Halo->AngMom[2]);*/
+    // vector_magnitude needs a double vector, not a float!!!
+    add_disks(gal, 1, cooling_mass,
+              vector_magnitude(newAngMom) / (2 * gal->Vvir),
+              gal->Vvir, AMcool);
+    /*add_disks(gal, 1, cooling_mass,
+              HaloAngMomMag / (2 * gal->Vvir),
+              gal->Vvir, AMcool);*/
+  }
 #endif
 
   // update the galaxy reservoirs
@@ -158,7 +160,8 @@ void cool_gas_onto_galaxy(galaxy_t* gal, double cooling_mass)
 #if USE_ANG_MOM
   /*increment_angular_momentum(gal->AMcold, gal->Halo->AngMom,
                              cooling_mass);*/
-  increment_angular_momentum(gal->AMcold, newAngMom,
-                             cooling_mass);                             
+  if (cooling_mass >= 1e-10)
+    increment_angular_momentum(gal->AMcold, newAngMom,
+                               cooling_mass);                             
 #endif
 }
